fd32ffee83f91c42086ce456896858faa07de1a0
Mirko Hecky <m.hecky@simon-ibv.de>
2017-04-03 17:16:47 +0200

    Added some needed files

---
+++ Readme.md
@@ -0,0 +1,7 @@
+
+## Installation
+In order to install the latest release of buildsystem-docker-msdev6, run the following command:
+
+```
+pip install https://github.com/mirhec/buildsystem-docker-msdev6/zipball/master
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='buildsystem-docker-msdev6',
+      version='1.0',
+      author='Mirko Hecky',
+      url='https://github.com/mirhec/buildsystem-docker-msdev6',
+      packages=['buildsystem'],
+)
