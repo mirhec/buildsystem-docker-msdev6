655fe5b1a2d726afcbd12fd9474355bcd6041b77
Mirko Hecky <m.hecky@simon-ibv.de>
2017-04-04 15:51:53 +0200

    Added some files for pypi upload

--- Readme.md
+++ Readme.md
@@ -3,5 +3,5 @@
 In order to install the latest release of buildsystem-docker-msdev6, run the following command:
 
 ```
-pip install https://github.com/mirhec/buildsystem-docker-msdev6/zipball/master
+pip install buildsystem-docker-msdev6
 ```
--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from distutils.core import setup
 
 setup(name='buildsystem-docker-msdev6',
       version='1.0',
+      description='Msdev6 plugin for buildsystem package.',
       author='Mirko Hecky',
       url='https://github.com/mirhec/buildsystem-docker-msdev6',
+      download_url='https://github.com/mirhec/buildsystem-docker-msdev6/archive/1.0.tar.gz',
       packages=['buildsystem'],
+      keywords=['buildsystem', 'build', 'automation', 'docker', 'msdev', 'msdev6', 'vc6']
 )
